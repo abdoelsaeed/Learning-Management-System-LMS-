edbadf1a9dd8f89ed15ccff3399e776d72836a07
abdoelsaeed <abdoelsaeed290@icloud.com>
2025-06-12 19:57:47 +0300

    add payment and update stripe

--- Database/data_source.js
+++ Database/data_source.js
@@ -1,13 +1,14 @@
 require("reflect-metadata");
 const { DataSource } = require("typeorm");
 const User = require("./../entities/UserEntity");
+console.log('User Entity Loaded:', User);
 const Course = require("./../entities/CourseEntity");
 const Enrollment = require("./../entities/EnrollmentEntity");
 const Payment = require("./../entities/PaymentEntity");
 const AppDataSource = new DataSource({
   type: "postgres",
   url: process.env.DATABASE_URL, // استخدم رابط الاتصال من متغير البيئة
-  synchronize: true,
+  synchronize: false,
   logging: false,
   entities: [User, Course, Enrollment, Payment],
   migrations: [],
