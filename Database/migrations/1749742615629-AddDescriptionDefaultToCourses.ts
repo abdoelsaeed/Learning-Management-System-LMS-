f42539fcf9a2b327ca9f1c653d511be6160ce1e9
abdoelsaeed <abdoelsaeed290@icloud.com>
2025-06-12 18:40:18 +0300

    add enrollment

---
+++ Database/migrations/1749742615629-AddDescriptionDefaultToCourses.ts
@@ -0,0 +1,11 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class AddDescriptionDefaultToCourses1749742615629 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
--- server.js
+++ server.js
@@ -17,3 +17,5 @@ AppDataSource.initialize()
   .catch((err) => {
     console.error("Error during Data Source initialization:", err);
   });
+
+module.exports = app;
